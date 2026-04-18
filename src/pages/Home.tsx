d3ca19c9f32b1abe6305030348aa8166686af8c5
LesiaPerebyinis <72079509+LesiaPerebyinis@users.noreply.github.com>
2026-04-18 17:42:04 +0300

    fix: update image paths and base url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <main className="container mx-auto px-4 sm:px-6 lg:px-8 flex-grow">
       <section>
-        <div> GJVBKRF</div>
         <PizzaList />
       </section>
     </main>
